685e5ea2efff476dcca3d14ddf1448350ca18721
Marciano111 <mgemz@DESKTOP-MKTAA21>
2023-07-24 20:39:00 +0200

    Zadanie domowe - Dzień 15

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,21 +1,21 @@
 ﻿using ChallengeApp;
 
-Console.WriteLine("Witamy w Programie BEST EMPLOYEE do oceny Pracowników");
-Console.WriteLine("=====================================================");
+Console.WriteLine("Witamy w Programie BEST SUPERVISOR do oceny Kadry Kierowniczej");
+Console.WriteLine("==============================================================");
 Console.Write("Podaj swoje imię: ");
 var name = Console.ReadLine();
 Console.WriteLine();
-Console.WriteLine($"Witaj {name}! Już za chwilę będziesz mógł ocenić Pracownika.");
+Console.WriteLine($"Witaj {name}! Już za chwilę będziesz mógł ocenić Kierownika.");
 Console.WriteLine();
 
-var employee = new Employee("Adam", "Nowak", 'M');
+var supervisor = new Supervisor("Jan", "Kowalski");
 
 while (true)
 {
-    Console.WriteLine("Podaj ocenę Pracownika (aby wyjść - wybierz 'q'): ");
+    Console.WriteLine("Podaj ocenę Kierownika (aby wyjść - wybierz 'q'): ");
     var input = Console.ReadLine();
 
-    if(input == "q")
+    if (input == "q")
     {
         Console.WriteLine();
         Console.WriteLine("Dodawanie ocen zakończone!");
@@ -24,7 +24,7 @@ while (true)
 
     try
     {
-        employee.AddGrade(input);
+        supervisor.AddGrade(input);
     }
     catch (Exception ex)
     {
@@ -32,16 +32,62 @@ while (true)
     }
 }
 
-var statistics = employee.GetStatistics();
+var statistics = supervisor.GetStatistics();
 
 Console.WriteLine();
-Console.WriteLine("Podsumowanie dot. ocen Pracownika.");
+Console.WriteLine("Podsumowanie dot. ocen Kierownika.");
 Console.WriteLine("==================================");
-Console.WriteLine($"Minimalna ocena pracownika:  {statistics.Min}");
-Console.WriteLine($"Maksymalna ocena pracownika: {statistics.Max}");
-Console.WriteLine($"Średnia ocena pracownika:    {statistics.Average:N2}");
+Console.WriteLine($"Minimalna ocena kierownika:  {statistics.Min}");
+Console.WriteLine($"Maksymalna ocena kierownika: {statistics.Max}");
+Console.WriteLine($"Średnia ocena kierownika:    {statistics.Average:N2}");
 Console.WriteLine("==================================");
 Console.WriteLine();
 Console.WriteLine("Dziękujemy za wystawione oceny.");
 Console.WriteLine("Dzięki Tobie stajemy się lepsi!");
 
+
+
+//Console.WriteLine("Witamy w Programie BEST EMPLOYEE do oceny Pracowników");
+//Console.WriteLine("=====================================================");
+//Console.Write("Podaj swoje imię: ");
+//var name = Console.ReadLine();
+//Console.WriteLine();
+//Console.WriteLine($"Witaj {name}! Już za chwilę będziesz mógł ocenić Pracownika.");
+//Console.WriteLine();
+
+//var employee = new Employee("Adam", "Nowak");
+
+//while (true)
+//{
+//    Console.WriteLine("Podaj ocenę Pracownika (aby wyjść - wybierz 'q'): ");
+//    var input = Console.ReadLine();
+
+//    if (input == "q")
+//    {
+//        Console.WriteLine();
+//        Console.WriteLine("Dodawanie ocen zakończone!");
+//        break;
+//    }
+
+//    try
+//    {
+//        employee.AddGrade(input);
+//    }
+//    catch (Exception ex)
+//    {
+//        Console.WriteLine($"Exception catched: {ex.Message}");
+//    }
+//}
+
+//var statistics = employee.GetStatistics();
+
+//Console.WriteLine();
+//Console.WriteLine("Podsumowanie dot. ocen Pracownika.");
+//Console.WriteLine("==================================");
+//Console.WriteLine($"Minimalna ocena pracownika:  {statistics.Min}");
+//Console.WriteLine($"Maksymalna ocena pracownika: {statistics.Max}");
+//Console.WriteLine($"Średnia ocena pracownika:    {statistics.Average:N2}");
+//Console.WriteLine("==================================");
+//Console.WriteLine();
+//Console.WriteLine("Dziękujemy za wystawione oceny.");
+//Console.WriteLine("Dzięki Tobie stajemy się lepsi!");
--- ChallengeApp/ChallengeApp/Supervisor.cs
+++ ChallengeApp/ChallengeApp/Supervisor.cs
@@ -1,16 +1,16 @@
 ﻿namespace ChallengeApp
 {
-    public class Employee : IEmployee
+    public class Supervisor : IEmployee
     {
         private List<float> grades = new List<float>();
 
-        public Employee(string name, string surname)
+        public Supervisor(string name, string surname)
         {
             this.Name = name;
             this.Surname = surname;
         }
 
-        public string Name { get; private set; }    
+        public string Name { get; private set; }
         public string Surname { get; private set; }
 
         public void AddGrade(float grade)
@@ -68,16 +68,71 @@
 
         public void AddGrade(string grade)
         {
-            if (float.TryParse(grade, out float result))
+            switch (grade)
             {
-                this.AddGrade(result);
-            }
-            else
-            {
-                throw new Exception("String is not float");
-            }
+                case "6":
+                    this.AddGrade(100);
+                    break;
+                case "-6":
+                case "6-":
+                    this.AddGrade(95);
+                    break;
+                case "+5":
+                case "5+":
+                    this.AddGrade(85);
+                    break;
+                case "5":
+                    this.AddGrade(80);
+                    break;
+                case "-5":
+                case "5-":
+                    this.AddGrade(75);
+                    break;
+                case "+4":
+                case "4+":
+                    this.AddGrade(65);
+                    break;
+                case "4":
+                    this.AddGrade(60);
+                    break;
+                case "-4":
+                case "4-":
+                    this.AddGrade(55);
+                    break;
+                case "+3":
+                case "3+":
+                    this.AddGrade(45);
+                    break;
+                case "3":
+                    this.AddGrade(40);
+                    break;
+                case "-3":
+                case "3-":
+                    this.AddGrade(35);
+                    break;
+                case "+2":
+                case "2+":
+                    this.AddGrade(25);
+                    break;
+                case "2":
+                    this.AddGrade(20);
+                    break;
+                case "-2":
+                case "2-":
+                    this.AddGrade(15);
+                    break;
+                case "+1":
+                case "1+":
+                    this.AddGrade(5);
+                    break;
+                case "1":
+                    this.AddGrade(0);
+                    break;
+                default:
+                    throw new Exception("Inncorrect grade!");
+             }
         }
-
+        
         public Statistics GetStatistics()
         {
             var statistics = new Statistics();
@@ -119,3 +174,4 @@
 
     }
 }
+

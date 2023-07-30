2c649851f0e4df25a65c0946e31a272ecc9eaacb
Marciano111 <mgemz@DESKTOP-MKTAA21>
2023-07-30 21:29:07 +0200

    Zadanie domowe - Dzień 16

---
+++ ChallengeApp/ChallengeApp/EmployeeInFile.cs
@@ -0,0 +1,132 @@
+﻿namespace ChallengeApp
+{
+    public class EmployeeInFile : EmployeeBase
+    {
+        private const string fileName = "grades.txt";
+
+        public EmployeeInFile(string name, string surname)
+            : base(name, surname)
+        {
+        }
+
+        public override void AddGrade(float grade)
+        {
+            if (grade >= 0 && grade <= 100)
+            {
+                using (var writer = File.AppendText(fileName))
+                {
+                    writer.WriteLine(grade);
+                }
+            }
+            else
+            {
+                throw new Exception("Invalid grade value");
+            }
+        }
+        
+        public override void AddGrade(double grade)
+        {
+            var doubleToFloat = (float)grade;
+            this.AddGrade(doubleToFloat);
+        }
+
+        public override void AddGrade(int grade)
+        {
+            var intToFloat = (float)grade;
+            this.AddGrade(intToFloat);
+        }
+
+        public override void AddGrade(char grade)
+        {
+            switch (grade)
+            {
+                case 'A':
+                case 'a':
+                    this.AddGrade(100);
+                    break;
+                case 'B':
+                case 'b':
+                    this.AddGrade(80);
+                    break;
+                case 'C':
+                case 'c':
+                    this.AddGrade(60);
+                    break;
+                case 'D':
+                case 'd':
+                    this.AddGrade(40);
+                    break;
+                case 'E':
+                case 'e':
+                    this.AddGrade(20);
+                    break;
+                default:
+                    throw new Exception("Wrong Letter");
+            }
+        }
+    
+        public override void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else
+            {
+                throw new Exception("String is not float");
+            }
+        }
+
+        public override Statistics GetStatistics()
+        {
+            var result = new Statistics();
+
+            if(File.Exists(fileName))
+            {
+                using (var reader = File.OpenText(fileName))
+                {
+                    var lineCounter = 0;
+                    var line = reader.ReadLine();
+
+                    result.Average = 0;
+                    result.Max = float.MinValue;
+                    result.Min = float.MaxValue;
+
+                    while (line != null)
+                    {
+                        var number = float.Parse(line);
+                        
+                        result.Max = Math.Max(result.Max, number);
+                        result.Min = Math.Min(result.Min, number);
+                        result.Average += number;
+                        lineCounter++;
+
+                        line = reader.ReadLine();
+                    }
+
+                    result.Average /= lineCounter;
+                    
+                    switch (result.Average)
+                    {
+                        case var average when average >= 80:
+                            result.AverageLetter = 'A';
+                            break;
+                        case var average when average >= 60:
+                            result.AverageLetter = 'B';
+                            break;
+                        case var average when average >= 40:
+                            result.AverageLetter = 'C';
+                            break;
+                        case var average when average >= 20:
+                            result.AverageLetter = 'D';
+                            break;
+                        default:
+                            result.AverageLetter = 'E';
+                            break;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,93 +1,17 @@
 ﻿using ChallengeApp;
 
-Console.WriteLine("Witamy w Programie BEST SUPERVISOR do oceny Kadry Kierowniczej");
-Console.WriteLine("==============================================================");
-Console.Write("Podaj swoje imię: ");
-var name = Console.ReadLine();
-Console.WriteLine();
-Console.WriteLine($"Witaj {name}! Już za chwilę będziesz mógł ocenić Kierownika.");
-Console.WriteLine();
+// Zapisywanie danych do pliku
+var employee = new EmployeeInFile("Adam", "Nowak");
 
-var supervisor = new Supervisor("Jan", "Kowalski");
+employee.AddGrade(1.0f);
+employee.AddGrade(2d);
+employee.AddGrade(3);
+employee.AddGrade('E'); // 20
+employee.AddGrade("4");
 
-while (true)
-{
-    Console.WriteLine("Podaj ocenę Kierownika (aby wyjść - wybierz 'q'): ");
-    var input = Console.ReadLine();
+// Odczytywanie danych z pliku
+var result = employee.GetStatistics();
 
-    if (input == "q")
-    {
-        Console.WriteLine();
-        Console.WriteLine("Dodawanie ocen zakończone!");
-        break;
-    }
-
-    try
-    {
-        supervisor.AddGrade(input);
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Exception catched: {ex.Message}");
-    }
-}
-
-var statistics = supervisor.GetStatistics();
-
-Console.WriteLine();
-Console.WriteLine("Podsumowanie dot. ocen Kierownika.");
-Console.WriteLine("==================================");
-Console.WriteLine($"Minimalna ocena kierownika:  {statistics.Min}");
-Console.WriteLine($"Maksymalna ocena kierownika: {statistics.Max}");
-Console.WriteLine($"Średnia ocena kierownika:    {statistics.Average:N2}");
-Console.WriteLine("==================================");
-Console.WriteLine();
-Console.WriteLine("Dziękujemy za wystawione oceny.");
-Console.WriteLine("Dzięki Tobie stajemy się lepsi!");
-
-
-
-//Console.WriteLine("Witamy w Programie BEST EMPLOYEE do oceny Pracowników");
-//Console.WriteLine("=====================================================");
-//Console.Write("Podaj swoje imię: ");
-//var name = Console.ReadLine();
-//Console.WriteLine();
-//Console.WriteLine($"Witaj {name}! Już za chwilę będziesz mógł ocenić Pracownika.");
-//Console.WriteLine();
-
-//var employee = new Employee("Adam", "Nowak");
-
-//while (true)
-//{
-//    Console.WriteLine("Podaj ocenę Pracownika (aby wyjść - wybierz 'q'): ");
-//    var input = Console.ReadLine();
-
-//    if (input == "q")
-//    {
-//        Console.WriteLine();
-//        Console.WriteLine("Dodawanie ocen zakończone!");
-//        break;
-//    }
-
-//    try
-//    {
-//        employee.AddGrade(input);
-//    }
-//    catch (Exception ex)
-//    {
-//        Console.WriteLine($"Exception catched: {ex.Message}");
-//    }
-//}
-
-//var statistics = employee.GetStatistics();
-
-//Console.WriteLine();
-//Console.WriteLine("Podsumowanie dot. ocen Pracownika.");
-//Console.WriteLine("==================================");
-//Console.WriteLine($"Minimalna ocena pracownika:  {statistics.Min}");
-//Console.WriteLine($"Maksymalna ocena pracownika: {statistics.Max}");
-//Console.WriteLine($"Średnia ocena pracownika:    {statistics.Average:N2}");
-//Console.WriteLine("==================================");
-//Console.WriteLine();
-//Console.WriteLine("Dziękujemy za wystawione oceny.");
-//Console.WriteLine("Dzięki Tobie stajemy się lepsi!");
+Console.WriteLine($"Minimalna ocena Pracownika:  {result.Min}");
+Console.WriteLine($"Maksymalna ocena Pracownika: {result.Max}");
+Console.WriteLine($"Średnia ocena Pracownika:    {result.Average:N2}");
